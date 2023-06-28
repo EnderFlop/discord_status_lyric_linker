5a0fcae727d3151913b7372d36f8e68ab88824a6
H4rl <98224660+h4rldev@users.noreply.github.com>
2023-06-28 10:31:08 +0000

    Gonna kill self

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
-import gevent.monkey
-gevent.monkey.patch_socket()
-import grequests
-import requests
-import time
-import fpstimer
 import os
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
-import threading
 import signal
 import sys
+import time
 
-load_dotenv()
+import fpstimer
+import gevent.monkey
+import grequests
+import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+gevent.monkey.patch_socket()
+
 API_TOKEN = os.environ.get("DISCORD_AUTH")  # https://discordhelp.net/discord-token
 SPOTIFY_ID = os.environ.get("SPOTIFY_ID")
 SPOTIFY_SECRET = os.environ.get("SPOTIFY_SECRET")
@@ -21,47 +20,48 @@ CUSTOM_STATUS = os.environ.get("STATUS")
 SCOPE = "user-read-currently-playing"
 TIMER = fpstimer.FPSTimer(2)
 
-def main(sp, last_played_song, last_played_line):
+
+def main(sp, last_played_song, line_last_played):
     try:
         song = sp.current_user_playing_track()
 
         # IF NO SONG IS PLAYING
         if not song:
-            if last_played_line == "NO SONG":
+            if line_last_played == "NO SONG":
                 return "", "NO SONG"
-            requests.patch(url="https://discord.com/api/v6/users/@me/settings",
-                           headers={"authorization": API_TOKEN},
-                           json={
-                               "custom_status": {
-                                   "text": CUSTOM_STATUS,
-                                   "emoji_name": ""
-                               }
-                           })
+            requests.patch(
+                url="https://discord.com/api/v6/users/@me/settings",
+                headers={"authorization": API_TOKEN},
+                json={"custom_status": {"text": CUSTOM_STATUS, "emoji_name": ""}},
+                timeout=10,
+            )
             TIMER.sleep()
             return "", "NO SONG"
 
         track_id = song["item"]["uri"].split(":")[-1]
         current_time = song["progress_ms"]
-        formatted_currently_playing = f"{song['item']['name']} -- {song['item']['artists'][0]['name']}"
-        lyrics = requests.get(f"https://spotify-lyric-api.herokuapp.com/?trackid={track_id}").json()
+        #formatted_currently_playing = (
+        #    f"{song['item']['name']} -- {song['item']['artists'][0]['name']}"
+        #)
+        lyrics = requests.get(
+            f"https://spotify-lyric-api.herokuapp.com/?trackid={track_id}", timeout=10
+        ).json()
         # these two requests take around 0.5 seconds.
 
         # IF THERE ARE NO LYRICS
-        if lyrics["error"] == True or lyrics["syncType"] == "UNSYNCED":
+        if lyrics["error"] is True or lyrics["syncType"] == "UNSYNCED":
             # If we've already been here this song, don't bother changing again, just return.
-            if last_played_line == "NO LYRICS":
-                return song['item']['name'], last_played_line
-            req = grequests.patch(url="https://discord.com/api/v6/users/@me/settings",
-                                  headers={"authorization": API_TOKEN},
-                                  json={
-                                      "custom_status": {
-                                          "text": "🎵",
-                                          "emoji_name": ""
-                                      }
-                                  })
+            if line_last_played == "NO LYRICS":
+                return song["item"]["name"], last_played_line
+            req = grequests.patch(
+                url="https://discord.com/api/v6/users/@me/settings",
+                headers={"authorization": API_TOKEN},
+                json={"custom_status": {"text": "🎵", "emoji_name": ""}},
+                timeout=10,
+            )
             grequests.send(req, grequests.Pool(1))
-            last_played_line = "NO LYRICS"
-            return song['item']['name'], last_played_line
+            line_last_played = "NO LYRICS"
+            return song["item"]["name"], line_last_played
 
         # IF THERE ARE LYRICS
         else:
@@ -73,43 +73,40 @@ def main(sp, last_played_song, last_played_line):
                     min_time = milliseconds_past_line
                     next_line = line["words"]
 
-            if last_played_line != next_line:  # no need to update if the line hasn't changed.
+            if (
+                line_last_played != next_line
+            ):  # no need to update if the line hasn't changed.
                 if next_line != "":
-                    status_req = grequests.patch(url="https://discord.com/api/v6/users/@me/settings",
-                                                headers={"authorization": API_TOKEN},
-                                                json={
-                                                    "custom_status": {
-                                                        "text": next_line,
-                                                        "emoji_name": ""
-                                                    }
-                                                })
+                    status_req = grequests.patch(
+                        url="https://discord.com/api/v6/users/@me/settings",
+                        headers={"authorization": API_TOKEN},
+                        json={"custom_status": {"text": next_line, "emoji_name": ""}},
+                        timeout=10,
+                    )
                     grequests.send(status_req, grequests.Pool(1))
                 else:
-                    status_req = grequests.patch(url="https://discord.com/api/v6/users/@me/settings",
-                                                headers={"authorization": API_TOKEN},
-                                                json={
-                                                    "custom_status": {
-                                                        "text": "🎵",
-                                                        "emoji_name": ""
-                                                    }
-                                                })
+                    status_req = grequests.patch(
+                        url="https://discord.com/api/v6/users/@me/settings",
+                        headers={"authorization": API_TOKEN},
+                        json={"custom_status": {"text": "🎵", "emoji_name": ""}},
+                        timeout=10,
+                    )
                     grequests.send(status_req, grequests.Pool(1))
-            last_played_line = next_line
-            return song['item']['name'], last_played_line
+            line_last_played = next_line
+            return song["item"]["name"], line_last_played
 
-    except (requests.exceptions.RequestException, spotipy.SpotifyException) as e:
-        print("An error occurred:", str(e))
+    except (requests.exceptions.RequestException, spotipy.SpotifyException) as error:
+        print("An error occurred:", str(error))
         raise
 
-def signal_handler(signal, frame):
-    requests.patch(url="https://discord.com/api/v6/users/@me/settings",
-                   headers={"authorization": API_TOKEN},
-                   json={
-                       "custom_status": {
-                           "text": CUSTOM_STATUS,
-                           "emoji_name": ""
-                       }
-                   })
+
+def signal_handler():
+    requests.patch(
+        url="https://discord.com/api/v6/users/@me/settings",
+        headers={"authorization": API_TOKEN},
+        json={"custom_status": {"text": CUSTOM_STATUS, "emoji_name": ""}},
+        timeout=10,
+    )
     sys.exit(0)
 
 
@@ -122,12 +119,17 @@ if __name__ == "__main__":
     else:
         TOKEN = auth.get_access_token(as_dict=False)
     sp = spotipy.Spotify(TOKEN)
-    signal.signal(signal.SIGINT, signal_handler)  # Register the signal handler for CTRL+C
+    signal.signal(
+        signal.SIGINT, signal_handler
+    )  # Register the signal handler for CTRL+C
     try:
         while True:
             # time is slept inside main()
-            last_played_song, last_played_line = main(sp, last_played_song, last_played_line)
+            last_played_song, last_played_line = main(
+                sp, last_played_song, last_played_line
+            )
     except Exception as e:
+        print(e)
         TOKEN = auth.get_cached_token()["access_token"]
         sp = spotipy.Spotify(TOKEN)
         print("Error!" + str(e) + "\nAlso reauthenticating Spotify.")
--- start.py
+++ start.py
@@ -1,29 +1,43 @@
 import os
-import sys
+import pathlib
 import platform
-import subprocess
 import signal
-import requests
+import subprocess
+import sys
 import time
 
-def venv():
-    os.system("python -m venv venv")
-    if platform.system() == "Windows":
-        pip_executable = "venv\\Scripts\\pip.exe"
-        python_executable = "venv\\Scripts\\python.exe"
-    else:
-        pip_executable = "venv/bin/pip"
-        python_executable = "venv/bin/python"
-    subprocess.run([pip_executable, "install", "grequests", "fpstimer", "spotipy", "python_dotenv", "gevent"])
-    subprocess.run([python_executable, "-m", "pip", "install", "--upgrade", "pip"])
+# N0v4 what kind of crack are you smoking?
 
-def create_env_file(token, client_id, client_secret, redirect_uri, status):
-    with open(".env", "w") as file:
-        file.write(f"DISCORD_AUTH = {token}\n")
-        file.write(f"SPOTIFY_ID = {client_id}\n")
-        file.write(f"SPOTIFY_SECRET = {client_secret}\n")
-        file.write(f"SPOTIFY_REDIRECT = {redirect_uri}\n")
-        file.write(f"STATUS = {status}\n")
+
+def venv():
+    if platform.system() != "Windows":
+        subprocess.run([sys.executable, "-m", "venv", "venv"])
+        subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+    pip_executable = pathlib.PurePath(sys.executable).parent / "pip.exe"
+    subprocess.run(
+        [
+            pip_executable,
+            "install",
+            "grequests",
+            "fpstimer",
+            "spotipy",
+            "python_dotenv",
+            "gevent",
+        ]
+    )
+    # subprocess.run([python_executable, "-m", "pip", "install", "--upgrade", "pip"])
+    # Doesn't this break shit on windows? Better to not update pip
+
+
+def create_env_file(creds: list):
+    """Create a .env file based on the credentials in the creds list."""
+    with open(".env", "w", encoding="utf-8") as file:
+        file.write(f"DISCORD_AUTH = {creds[0]}\n")
+        file.write(f"SPOTIFY_ID = {creds[1]}\n")
+        file.write(f"SPOTIFY_SECRET = {creds[2]}\n")
+        file.write(f"SPOTIFY_REDIRECT = {creds[3]}\n")
+        file.write(f"STATUS = {creds[4]}\n")
+
 
 def main():
     if not os.path.isfile(".env"):
@@ -31,14 +45,23 @@ def main():
         spotify_client_id = input("Enter Spotify application client ID: ")
         spotify_client_secret = input("Enter Spotify application client secret: ")
         spotify_redirect_uri = input("Enter Spotify application redirect URI: ")
-        custom_status = input("Enter custom status (shows when there is no lyrics/no song is playing): ")
-        create_env_file(discord_token, spotify_client_id, spotify_client_secret, spotify_redirect_uri, custom_status)
+        custom_status = input(
+            "Enter custom status (shows when there is no lyrics/no song is playing): "
+        )
+        information = [
+            discord_token,
+            spotify_client_id,
+            spotify_client_secret,
+            spotify_redirect_uri,
+            custom_status,
+        ]
+        create_env_file(information)
 
     venv()
     print("Initialized, starting...")
     while True:
         try:
-            process = subprocess.Popen(["./venv/bin/python", "bot.py"])
+            process = subprocess.Popen([sys.executable, "bot.py"])
             process.wait()
             print("Restarting because script crashed...")
         except KeyboardInterrupt:
@@ -46,14 +69,15 @@ def main():
 
     time.sleep(1)
 
-    def signal_handler(signal, frame):
-        process.send_signal(signal.SIGINT)
+    def signal_handler(term_signal):
+        process.send_signal(term_signal.SIGINT)
         process.wait()
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
 
     process.wait()
+
 
 if __name__ == "__main__":
     main()
